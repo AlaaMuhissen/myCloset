8ce9cd29c5a3a61c055a88b5da0788b798a57061
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2023-12-15 18:46:01 +0000

    Constraints to use the rear camera

--- myCloset/src/Components/addClothes/TakePhoto.jsx
+++ myCloset/src/Components/addClothes/TakePhoto.jsx
@@ -1,10 +1,10 @@
-import React, { useRef ,useCallback ,useState } from 'react';
-import Webcam from "react-webcam";
+import React, { useRef, useCallback, useState } from 'react';
+import Webcam from 'react-webcam';
 
 export default function TakePhoto() {
-
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
+
   const retake = () => {
     setImgSrc(null);
   };
@@ -13,23 +13,34 @@ export default function TakePhoto() {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
   }, [webcamRef]);
+
+  // Constraints to use the rear camera
+  const videoConstraints = {
+    facingMode: 'environment', // Set to 'environment' to use the rear camera
+  };
+
   return (
     <>
-    <h1>Hii</h1>
-    <div className="container">
-      {imgSrc ? (
-        <img src={imgSrc} alt="webcam" />
-      ) : (
-        <Webcam height={600} width={600} ref={webcamRef} />
-      )}
-      <div className="btn-container">
+      <h1>Hii</h1>
+      <div className="container">
         {imgSrc ? (
-          <button onClick={retake}>Retake photo</button>
+          <img src={imgSrc} alt="webcam" />
         ) : (
-          <button onClick={capture}>Capture photo</button>
+          <Webcam
+            height={600}
+            width={600}
+            ref={webcamRef}
+            videoConstraints={videoConstraints} // Apply the constraints here
+          />
         )}
+        <div className="btn-container">
+          {imgSrc ? (
+            <button onClick={retake}>Retake photo</button>
+          ) : (
+            <button onClick={capture}>Capture photo</button>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
-};
+}
