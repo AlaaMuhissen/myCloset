07a306b8f165a762caae932c47e06ac816a2b92f
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-01-19 19:08:13 +0000

    edit the weather API link

--- myCloset/src/Components/GetWeather.jsx
+++ myCloset/src/Components/GetWeather.jsx
@@ -12,10 +12,11 @@ export default function GetWeather() {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'http://api.weatherapi.com/v1/forecast.json?key=8f3dd32daa694e7f9b4150045231512&q=Jerusalem&days=7&aqi=no&alerts=no'
+          'https://api.weatherapi.com/v1/forecast.json?key=8f3dd32daa694e7f9b4150045231512&q=Jerusalem&days=7&aqi=no&alerts=no'
         );
         setWeatherData(response.data);
       } catch (error) {
+        console.log(error)
         setError('Error fetching weather data');
       } finally {
         setLoading(false);
